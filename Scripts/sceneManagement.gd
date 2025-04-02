4895113c79fd340e59af86cf1f31d49bb99014ef
Pandawaflez <179869288+Pandawaflez@users.noreply.github.com>
2025-04-01 21:37:36 -0700

    Working on basement scene

--- Scripts/sceneManagement.gd
+++ Scripts/sceneManagement.gd
@@ -3,7 +3,7 @@ extends Resource
 
 @export var testScene0:String = "res://TestScene/locations/TestSceneLevel1.tscn"
 @export var testSceneAttic:String = "res://TestScene/locations/TestSceneLevel1Attic.tscn"
-@export var testSceneBasement:String = ""
+@export var testSceneBasement:String = "res://TestScene/locations/testSceneLevel1Basement.tscn"
 
 @export var entrance:int = 0
 
